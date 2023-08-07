21d5dd90c8f298ba0fb5c39f78593505388ae386
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-08-07 10:37:42 +0530

    server started on port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.get('/', (req,res) => res.send('Hello'))
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, async () => {
+//   await connection;
+  console.log(`Server started on http://localhost:${PORT}`);
+});
 // software
